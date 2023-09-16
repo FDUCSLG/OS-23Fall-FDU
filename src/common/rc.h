b4a1d5a8972d810273100f025cc55dbf590a7f9e
idlebo <shili_c@163.com>
2023-09-16 21:24:15 +0800

    fix rc

--- src/common/rc.h
+++ src/common/rc.h
@@ -7,16 +7,14 @@ typedef struct {
     isize count;
 } RefCount;
 
-void _init_rc(RefCount*);
 void _increment_rc(RefCount*);
 bool _decrement_rc(RefCount*);
 
 // initialize reference count to zero.
-#define init_rc(checker, rc) _init_rc(rc)
-
+void init_rc(RefCount*);
 // atomic increment reference count by one.
-#define increment_rc(checker, rc) (checker_begin_ctx(checker), _increment_rc(rc))
+#define increment_rc(checker, rc) checker_begin_ctx_before_call(checker, _increment_rc, rc)
 
 // atomic decrement reference count by one. Returns true if reference
 // count becomes zero or below.
-#define decrement_rc(checker, rc) (checker_end_ctx(checker), _decrement_rc(rc))
+#define decrement_rc(checker, rc) checker_end_ctx_after_call(checker, _decrement_rc, rc)
