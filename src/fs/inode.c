93e0240c9ac7fd4b48c4b657e4495b12f51f1ac9
w568w <1278297578@qq.com>
2023-11-23 14:51:00 +0800

    lab: finish lab7 (code part)

---
+++ src/fs/inode.c
@@ -0,0 +1,230 @@
+#include <common/string.h>
+#include <fs/inode.h>
+#include <kernel/mem.h>
+#include <kernel/printk.h>
+
+/**
+    @brief the private reference to the super block.
+
+    @note we need these two variables because we allow the caller to
+            specify the block device and super block to use.
+            Correspondingly, you should NEVER use global instance of
+            them.
+
+    @see init_inodes
+ */
+static const SuperBlock* sblock;
+
+/**
+    @brief the reference to the underlying block cache.
+ */
+static const BlockCache* cache;
+
+/**
+    @brief global lock for inode layer.
+
+    Use it to protect anything you need.
+
+    e.g. the list of allocated blocks, ref counts, etc.
+ */
+static SpinLock lock;
+
+/**
+    @brief the list of all allocated in-memory inodes.
+
+    We use a linked list to manage all allocated inodes.
+
+    You can implement your own data structure if you like better performance.
+
+    @see Inode
+ */
+static ListNode head;
+
+
+// return which block `inode_no` lives on.
+static INLINE usize to_block_no(usize inode_no) {
+    return sblock->inode_start + (inode_no / (INODE_PER_BLOCK));
+}
+
+// return the pointer to on-disk inode.
+static INLINE InodeEntry* get_entry(Block* block, usize inode_no) {
+    return ((InodeEntry*)block->data) + (inode_no % INODE_PER_BLOCK);
+}
+
+// return address array in indirect block.
+static INLINE u32* get_addrs(Block* block) {
+    return ((IndirectBlock*)block->data)->addrs;
+}
+
+// initialize inode tree.
+void init_inodes(const SuperBlock* _sblock, const BlockCache* _cache) {
+    init_spinlock(&lock);
+    init_list_node(&head);
+    sblock = _sblock;
+    cache = _cache;
+
+    if (ROOT_INODE_NO < sblock->num_inodes)
+        inodes.root = inodes.get(ROOT_INODE_NO);
+    else
+        printk("(warn) init_inodes: no root inode.\n");
+}
+
+// initialize in-memory inode.
+static void init_inode(Inode* inode) {
+    init_sleeplock(&inode->lock);
+    init_rc(&inode->rc);
+    init_list_node(&inode->node);
+    inode->inode_no = 0;
+    inode->valid = false;
+}
+
+// see `inode.h`.
+static usize inode_alloc(OpContext* ctx, InodeType type) {
+    ASSERT(type != INODE_INVALID);
+
+    // TODO
+    return 0;
+}
+
+// see `inode.h`.
+static void inode_lock(Inode* inode) {
+    ASSERT(inode->rc.count > 0);
+    // TODO
+}
+
+// see `inode.h`.
+static void inode_unlock(Inode* inode) {
+    ASSERT(inode->rc.count > 0);
+    // TODO
+}
+
+// see `inode.h`.
+static void inode_sync(OpContext* ctx, Inode* inode, bool do_write) {
+    // TODO
+}
+
+// see `inode.h`.
+static Inode* inode_get(usize inode_no) {
+    ASSERT(inode_no > 0);
+    ASSERT(inode_no < sblock->num_inodes);
+    _acquire_spinlock(&lock);
+    // TODO
+    return NULL;
+}
+// see `inode.h`.
+static void inode_clear(OpContext* ctx, Inode* inode) {
+    // TODO
+}
+
+// see `inode.h`.
+static Inode* inode_share(Inode* inode) {
+    // TODO
+    return 0;
+}
+
+// see `inode.h`.
+static void inode_put(OpContext* ctx, Inode* inode) {
+    // TODO
+}
+
+/**
+    @brief get which block is the offset of the inode in.
+
+    e.g. `inode_map(ctx, my_inode, 1234, &modified)` will return the block
+    number of the block that contains the 1234th byte of the file
+    represented by `my_inode`.
+
+    If a block has not been allocated for that byte, `inode_map` will
+    allocate a new block and update `my_inode`, at which time, `modified`
+    will be set to true.
+
+    HOWEVER, if `ctx == NULL`, `inode_map` will NOT try to allocate any new block,
+    and when it finds that the block has not been allocated, it will return 0.
+    
+    @param[out] modified true if some new block is allocated and `inode`
+    has been changed.
+
+    @return usize the block number of that block, or 0 if `ctx == NULL` and
+    the required block has not been allocated.
+
+    @note the caller must hold the lock of `inode`.
+ */
+static usize inode_map(OpContext* ctx,
+                       Inode* inode,
+                       usize offset,
+                       bool* modified) {
+    // TODO
+    return 0;
+}
+
+// see `inode.h`.
+static usize inode_read(Inode* inode, u8* dest, usize offset, usize count) {
+    InodeEntry* entry = &inode->entry;
+    if (count + offset > entry->num_bytes)
+        count = entry->num_bytes - offset;
+    usize end = offset + count;
+    ASSERT(offset <= entry->num_bytes);
+    ASSERT(end <= entry->num_bytes);
+    ASSERT(offset <= end);
+
+    // TODO
+    return 0;
+}
+
+// see `inode.h`.
+static usize inode_write(OpContext* ctx,
+                         Inode* inode,
+                         u8* src,
+                         usize offset,
+                         usize count) {
+    InodeEntry* entry = &inode->entry;
+    usize end = offset + count;
+    ASSERT(offset <= entry->num_bytes);
+    ASSERT(end <= INODE_MAX_BYTES);
+    ASSERT(offset <= end);
+
+    // TODO
+    return 0;
+}
+
+// see `inode.h`.
+static usize inode_lookup(Inode* inode, const char* name, usize* index) {
+    InodeEntry* entry = &inode->entry;
+    ASSERT(entry->type == INODE_DIRECTORY);
+
+    // TODO
+    return 0;
+}
+
+// see `inode.h`.
+static usize inode_insert(OpContext* ctx,
+                          Inode* inode,
+                          const char* name,
+                          usize inode_no) {
+    InodeEntry* entry = &inode->entry;
+    ASSERT(entry->type == INODE_DIRECTORY);
+
+    // TODO
+    return 0;
+}
+
+// see `inode.h`.
+static void inode_remove(OpContext* ctx, Inode* inode, usize index) {
+    // TODO
+}
+
+InodeTree inodes = {
+    .alloc = inode_alloc,
+    .lock = inode_lock,
+    .unlock = inode_unlock,
+    .sync = inode_sync,
+    .get = inode_get,
+    .clear = inode_clear,
+    .share = inode_share,
+    .put = inode_put,
+    .read = inode_read,
+    .write = inode_write,
+    .lookup = inode_lookup,
+    .insert = inode_insert,
+    .remove = inode_remove,
+};
---
+++ src/fs/inode.h
@@ -0,0 +1,269 @@
+#pragma once
+#include <common/list.h>
+#include <common/rc.h>
+#include <common/spinlock.h>
+#include <fs/cache.h>
+#include <fs/defines.h>
+
+/**
+    @brief the number of the root inode (i.e. the inode_no of `/`).
+ */
+#define ROOT_INODE_NO 1
+
+struct InodeTree;
+
+/**
+    @brief an inode in memory.
+
+    You can compare it to a `Block` because they have similar operating ways.
+
+    @see Block
+ */
+typedef struct {
+    /**
+        @brief the lock protecting the inode metadata and its content.
+
+        @note it does NOT protect `rc`, `node`, `valid`, etc, because they are
+        "runtime" variables, not filesystem "metadata" or "data" of the inode.
+     */
+    SleepLock lock;
+
+    /**
+        @brief the reference count of this inode.
+
+        Different from `Block`, an inode can be shared by multiple threads or
+        processes. So we need a reference count to track the number of
+        references to this inode.
+     */
+    RefCount rc;
+
+    /**
+        @brief list this inode into a linked list.
+     */
+    ListNode node;
+
+    /**
+        @brief the corresponding inode number on disk.
+
+        @note distinguish it from `block_no` in `Block`, which is the "block number".
+
+        `inode_no` should be the offset in block from the beginning of the inode area.
+     */
+    usize inode_no;
+
+    /**
+        @brief has the `entry` been loaded from disk?
+     */
+    bool valid;
+
+    /**
+        @brief the real in-memory copy of the inode on disk.
+     */
+    InodeEntry entry; 
+} Inode;
+
+/**
+    @brief interface of inode layer.
+ */
+typedef struct {
+    /**
+        @brief the root inode of the file system.
+
+        @see `init_inodes` should initialize it to a valid inode.
+     */
+    Inode* root;
+
+    /**
+        @brief allocate a new zero-initialized inode on disk.
+        
+        @param type the type of the inode to allocate.
+
+        @return the number of newly allocated inode.
+
+        @throw panic if allocation fails (e.g. no more free inode).
+     */
+    usize (*alloc)(OpContext* ctx, InodeType type);
+
+    /**
+        @brief acquire the sleep lock of `inode`.
+        
+        This method should be called before any write operation to `inode` and its
+        file content.
+
+        @throw panic if `inode` is not valid.
+
+        @see `unlock` - the counterpart of this method.
+     */
+    void (*lock)(Inode* inode);
+
+    /**
+        @brief release the sleep lock of `inode`.
+        
+        @see `lock` - the counterpart of this method.
+     */
+    void (*unlock)(Inode* inode);
+
+    /**
+        @brief synchronize the content of `inode` between memory and disk.
+        
+        Different from block cache, this method can either read or write the inode.
+
+        If `do_write` is true and the inode is valid, write the content of `inode` to disk.
+
+        If `do_write` is false and the inode is invalid, read the content of `inode` from disk.
+
+        If `do_write` is false and the inode is valid, do nothing.
+
+        @note here "write to disk" means "sync with block cache", not "directly
+        write to underneath SD card".
+
+        @note caller must hold the lock of `inode`.
+
+        @throw panic if `do_write` is true and `inode` is invalid.
+     */
+    void (*sync)(OpContext* ctx, Inode* inode, bool do_write);
+
+    /**
+        @brief get an inode by its inode number.
+        
+        This method should increment the reference count of the inode by one.
+
+        @note it does not load the inode from disk if it is not loaded
+        before!
+
+        @see `sync` will be responsible to load the content of inode.
+        
+        @return the `inode` of `inode_no`. `inode->valid` can be false.
+
+        @see `put` - the counterpart of this method.
+     */
+    Inode* (*get)(usize inode_no);
+
+    /**
+        @brief truncate all contents of `inode`.
+        
+        This method removes (i.e. "frees") all file blocks of `inode`.
+
+        @note do not forget to reset related metadata of `inode`, e.g. `inode->entry.num_bytes`.
+
+        @note caller must hold the lock of `inode`.
+     */
+    void (*clear)(OpContext* ctx, Inode* inode);
+
+    /**
+        @brief duplicate an inode.
+        
+        Call this if you want to share an inode with others.
+
+        It should increment the reference count of `inode` by one.
+
+        @return the duplicated inode (i.e. may just return `inode`).
+     */
+    Inode* (*share)(Inode* inode);
+
+    /**
+        @brief notify that you no longer need `inode`.
+        
+        This method is also responsible to free the inode if no one needs it:
+
+        "No one needs it" means it is useless BOTH in-memory (`inode->rc == 0`) and on-disk
+        (`inode->entry.num_links == 0`).
+
+        "Free the inode" means freeing all related file blocks and the inode itself.
+
+        @note do not forget `kfree(inode)` after you have done them all!
+
+        @note caller must NOT hold the lock of `inode`.
+
+        @see `get` - the counterpart of this method.
+
+        @see `clear` can be used to free all file blocks of `inode`.
+     */
+    void (*put)(OpContext* ctx, Inode* inode);
+
+    /**
+        @brief read `count` bytes from `inode`, beginning at `offset`, to `dest`.
+        
+        @return how many bytes you actually read.
+
+        @note caller must hold the lock of `inode`.
+     */
+    usize (*read)(Inode* inode, u8* dest, usize offset, usize count);
+
+    /**
+        @brief write `count` bytes from `src` to `inode`, beginning at `offset`.
+        
+        @return how many bytes you actually write.
+
+        @note caller must hold the lock of `inode`.
+     */
+    usize (*write)(OpContext* ctx,
+                   Inode* inode,
+                   u8* src,
+                   usize offset,
+                   usize count);
+
+    /**
+        @brief look up an entry named `name` in directory `inode`.
+
+        @param[out] index the index of found entry in this directory.
+
+        @return the inode number of the corresponding inode, or 0 if not found.
+        
+        @note caller must hold the lock of `inode`.
+
+        @throw panic if `inode` is not a directory.
+     */
+    usize (*lookup)(Inode* inode, const char* name, usize* index);
+
+    /**
+        @brief insert a new directory entry in directory `inode`.
+        
+        Add a new directory entry in `inode` called `name`, which points to inode 
+        with `inode_no`.
+
+        @return the index of new directory entry, or -1 if `name` already exists.
+
+        @note if the directory inode is full, you should grow the size of directory inode.
+
+        @note you do NOT need to change `inode->entry.num_links`. Another function
+        to be finished in our final lab will do this.
+
+        @note caller must hold the lock of `inode`.
+
+        @throw panic if `inode` is not a directory.
+     */
+    usize (*insert)(OpContext* ctx,
+                    Inode* inode,
+                    const char* name,
+                    usize inode_no);
+
+    /**
+        @brief remove the directory entry at `index`.
+        
+        If the corresponding entry is not used before, `remove` does nothing.
+
+        @note if the last entry is removed, you can shrink the size of directory inode.
+        If you like, you can also move entries to fill the hole.
+
+        @note caller must hold the lock of `inode`.
+
+        @throw panic if `inode` is not a directory.
+     */
+    void (*remove)(OpContext* ctx, Inode* inode, usize index);
+} InodeTree;
+
+/**
+    @brief the global inode layer instance.
+ */
+extern InodeTree inodes;
+
+/**
+    @brief initialize the inode layer.
+
+    @note do not forget to read the root inode from disk!
+
+    @param sblock the loaded super block.
+    @param cache the initialized block cache.
+ */
+void init_inodes(const SuperBlock* sblock, const BlockCache* cache);
