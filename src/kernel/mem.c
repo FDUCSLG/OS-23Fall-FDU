60c70e96f30c8a166bffc4d04129e972303acc12
singularity <12184989+singularity-s0@users.noreply.github.com>
2023-12-01 20:47:52 +0800

    fix missing function

--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -1,14 +1,14 @@
 #include "aarch64/intrinsic.h"
 #include "kernel/printk.h"
+#include <aarch64/mmu.h>
+#include <common/checker.h>
+#include <common/defines.h>
+#include <common/list.h>
 #include <common/rc.h>
 #include <common/spinlock.h>
-#include <common/checker.h>
+#include <driver/memlayout.h>
 #include <kernel/init.h>
 #include <kernel/mem.h>
-#include <driver/memlayout.h>
-#include <aarch64/mmu.h>
-#include <common/list.h>
-#include <common/defines.h>
 
 #define K_DEBUG 0
 
@@ -21,33 +21,28 @@
 
 RefCount alloc_page_cnt;
 
-define_early_init(alloc_page_cnt)
-{
-    init_rc(&alloc_page_cnt);
-}
+define_early_init(alloc_page_cnt) { init_rc(&alloc_page_cnt); }
 
-static QueueNode* pages;
+static QueueNode *pages;
 extern char end[];
-define_early_init(pages)
-{
-    for (u64 p = PAGE_BASE((u64)&end) + PAGE_SIZE; p < P2K(PHYSTOP); p += PAGE_SIZE)
-	   add_to_queue(&pages, (QueueNode*)p); 
+define_early_init(pages) {
+    for (u64 p = PAGE_BASE((u64)&end) + PAGE_SIZE; p < P2K(PHYSTOP);
+         p += PAGE_SIZE)
+        add_to_queue(&pages, (QueueNode *)p);
 }
 
-
-
-void* kalloc_page()
-{
+void *kalloc_page() {
     _increment_rc(&alloc_page_cnt);
     // TODO
     return fetch_from_queue(&pages);
 }
 
-void kfree_page(void* p)
-{
+void kfree_page(void *p) {
     _decrement_rc(&alloc_page_cnt);
     // TODO
-    add_to_queue(&pages, (QueueNode*)p);
+    add_to_queue(&pages, (QueueNode *)p);
 }
 
 // TODO: kalloc kfree
+
+u64 left_page_cnt() { return PAGE_COUNT - alloc_page_cnt.count; }
--- src/kernel/mem.h
+++ src/kernel/mem.h
@@ -5,9 +5,13 @@
 #include <common/list.h>
 #include <common/rc.h>
 
+#define PAGE_COUNT ((P2K(PHYSTOP) - PAGE_BASE((u64) & end)) / PAGE_SIZE - 1)
+
 struct page {
     RefCount ref;
 };
+
+u64 left_page_cnt();
 
 WARN_RESULT void *get_zero_page();
 
